52c2cfce035c1226be6c42693a252fc1c0d95779
eme2119@gmail.com <eme2119@gmail.com>
2022-11-16 22:25:31 -0600

    updated on package url

--- Microservice/APIGateway/src/main/java/apigateway/APIGatewayApplication.java
+++ Microservice/APIGateway/src/main/java/apigateway/APIGatewayApplication.java
@@ -1,4 +1,4 @@
-package Microservice.APIGateway.src.main.java.apigateway;
+package apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
